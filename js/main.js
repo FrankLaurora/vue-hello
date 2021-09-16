5871853422cb477be417b8363f2c4bb48663e6e9
FrankLaurora <francesco.laurora9@gmail.com>
2021-09-16 17:43:43 +0200

    Add some style, struggling with function on click.

--- js/main.js
+++ js/main.js
@@ -3,8 +3,20 @@ const app = new Vue (
         el: "#root",
 
         data: {
-            title : "Hello Vue!",
-            picSource : "https://pbs.twimg.com/media/Db3060FWkAAtlRV.jpg"
+            title: "Hello Vue!",
+            picSource: "https://pbs.twimg.com/media/Db3060FWkAAtlRV.jpg",
+            highlighted: "off",
         },
+
+        methods: {
+            highlight: function() {
+                if(this.highlighted == "off") {
+                    this.highlighted == "highlighted";
+                } else {
+                    this.highlighted == "off";
+                }
+                return this.highlighted;
+            },
+        }
     }
 );
