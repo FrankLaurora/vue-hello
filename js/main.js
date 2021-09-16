89c0c26125a0f3d3782be617171c79547fbd67e2
FrankLaurora <francesco.laurora9@gmail.com>
2021-09-16 16:28:47 +0200

    Add project

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const app = new Vue (
+    {
+        el: "#root",
+
+        data: {
+            title : "Hello Vue!",
+            picSource : "https://pbs.twimg.com/media/Db3060FWkAAtlRV.jpg"
+        },
+    }
+);
